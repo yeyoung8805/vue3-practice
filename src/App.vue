3ef5cb1f97e337aa17d14c86e8228b683427f5ec
yeyoung8805 <yyk8805@naver.com>
2022-08-25 16:54:58 +0900

    initial setting of App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
-  <h1>{{ message }}</h1>
-  <HelloWorld />
+  <h1>{{ count }}</h1>
 </template>
 
 <script>
-import HelloWorld from "~/components/HelloWorld.vue";
 export default {
-  components: {
-    HelloWorld,
-  },
   data() {
     return {
-      message: "Hello Vue!!!",
+      count: 0,
     };
   },
 };
 </script>
+
+<style></style>
